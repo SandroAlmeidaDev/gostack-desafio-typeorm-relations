58fc91b915f9459d6bc9760e2f1f390018e98063
SandroAlmeidaDev <sandroalmeida25@hotmail.com>
2020-07-11 21:09:03 -0300

    Control and services created for customers

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,26 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const checkCustomerExists = await this.customersRepository.findByEmail(
+      email,
+    );
+
+    if (checkCustomerExists) {
+      throw new AppError('This email is already registered');
+    }
+
+    const newCustomer = await this.customersRepository.create({
+      email,
+      name,
+    });
+
+    return newCustomer;
   }
 }
 
